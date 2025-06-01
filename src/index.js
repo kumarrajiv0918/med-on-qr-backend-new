21b350cde802daaf7624bde3cb8c4ab3fba5aed1
Mdfarhan2 <mdfarhan735290@gmail.com>
2025-06-01 16:30:53 +0530

    upload backend code

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ const dotenv = require('dotenv');
 const fileRoutes = require('./routes/file/fileRoutes');
 const authRoutes = require('./routes/auth/authRoutes');
 const connectDB = require('./config/db');
-const registerRoutes = require('./routes/auth/registerRoutes')
-const getAllUsersRoutes = require('./routes/auth/getAllUserRoutes')
+const registerRoutes = require('./routes/auth/registerRoutes');
+const getAllUsersRoutes = require('./routes/auth/getAllUserRoutes');
+
 const app = express();
 dotenv.config();
+
 const PORT = process.env.PORT || 3002;
+const HOST = process.env.HOST || '0.0.0.0';
+const CLIENT_ORIGIN = process.env.BASE_URL || 'http://localhost:3000/'; 
 connectDB();
-app.use(cors());
+
+app.use(cors({
+  origin: CLIENT_ORIGIN,
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
@@ -18,6 +27,8 @@ app.use('/uploadFile', fileRoutes);
 app.use('/auth', authRoutes);
 app.use('/api', registerRoutes);
 app.use('/getAlluserApi', getAllUsersRoutes);
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+
+// ✅ Listen with HOST and PORT
+app.listen(PORT, HOST, () => {
+  console.log(`✅ Server running at http://${HOST}:${PORT}`);
 });
