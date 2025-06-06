dcac236ad37973ade0a4bf0af653f0e9061b0d07
Mukut Prasad <mukutprasad1996@gmail.com>
2025-06-06 16:15:04 +0530

    Worked on change apssword

--- src/routes/auth/authRoutes.js
+++ src/routes/auth/authRoutes.js
@@ -3,9 +3,10 @@ const express = require('express');
 const authController = require('../../controllers/auth/authController');
 const statusUpdate = require('../../controllers/auth/statusUpdateController')
 const router = express.Router();
-const authentication = require('../../middlewares/authentication'); 
-
+const authentication = require('../../middlewares/authentication');
+const changePasswordController = require('../../controllers/auth/changePassword')
 router.post('/login', authController.login);
-router.put('/updateUserStatus/:id',authentication, statusUpdate.updateUserStatus);
+router.put('/updateUserStatus/:id', authentication, statusUpdate.updateUserStatus);
+router.post('/changePassword', authentication, changePasswordController.changePassword)
 
 module.exports = router;
